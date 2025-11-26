5d23eabfd2ee162b53a027f97ae1f06cc39fac9b
gabyesparza1429-lang <gabyesparza1429@gmail.com>
2025-11-26 16:19:31 -0600

    Update urls.py

--- AulaKitPro_Web/AulaKitPro_Core/urls.py
+++ AulaKitPro_Web/AulaKitPro_Core/urls.py
@@ -14,11 +14,12 @@ urlpatterns = [
     # en 143.198.138.195/usuarios/
     path('', include('usuarios.urls')), 
     
-    path('pagos/', include('pagos.urls')), 
+    #path('pagos/', include('pagos.urls')),# 
 ]
 
 # Configuración para servir archivos estáticos/media en entorno de desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     # Si tienes archivos de medios (imágenes subidas)
+
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
