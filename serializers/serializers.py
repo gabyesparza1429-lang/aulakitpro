71767898a666aadf8a26bf5177301f0d0cd1e40f
gabyesparza1429-lang <gabyesparza1429@gmail.com>
2025-11-27 05:32:02 -0600

    Delete serializers directory

--- serializers/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from usuarios.models import Usuario
-
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ["id", "username", "email", "first_name", "last_name"]
